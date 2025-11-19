6b902b343148c621b32cb97217e17ff3d9024199
devops team <devopsteam95@gmail.com>
2025-11-19 06:48:21 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.borjan.com.pk',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
