ce50934015b0b2f40f16c2c08673ffc2dd2b8a84
devops team <devopsteam95@gmail.com>
2025-11-19 07:27:27 +0000

    we aslo want add give the shop id they will add and aslo we will add

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -118,6 +118,7 @@ export default function RegisterPage() {
         role: role, 
         shopId: values.shopId || null,
         createdAt: serverTimestamp(),
+        imageUrl: '',
       });
       
       toast({
